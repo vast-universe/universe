ee611e358f3eca8d617493e6b1c369c02519d095
黄鹏 <huangpeng@huangpengdeMacBook-Pro.local>
2024-01-06 04:00:23 +0800

    ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	base: '/',
 	site: "https://vast-universe.github.io/universe/",
+	trailingSlash:'never',
 	integrations: [tailwind()],
+	image: {
+		service: squooshImageService(),
+	},
 	vite: {
 		build: {
 			rollupOptions: {
