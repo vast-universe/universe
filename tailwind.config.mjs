912d8813a8598665454c7b9b8ae0244fadd8ef47
黄鹏 <huangpeng@huangpengdeMacBook-Pro.local>
2024-01-06 01:40:59 +0800

    首页

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,4 +2,17 @@
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			transitionProperty: {
+				'colors': 'color', // 将 'colors' 映射到 'color'，表示颜色过渡
+			},
+			transitionDuration: {
+				'500': '500ms', // 配置颜色过渡的持续时间
+			},
+			transitionTimingFunction: {
+				'ease-in-out': 'ease-in-out', // 配置颜色过渡的缓动函数
+			},
+		},
+	},
 }
