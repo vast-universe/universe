6c6d6d2546133972193dc81dd6edd2cee4b497bd
黄鹏 <huangpeng@huangpengdeMacBook-Pro.local>
2024-01-06 17:45:43 +0800

    github图标

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	base: './',
-	site: "https://vast-universe.github.io/universe/",
+	site: "https://www.universe-hub.cn/",
 	trailingSlash: 'never',
 	integrations: [tailwind()],
 	image: {
