60974ff559a9375a73cb56690345e932aa182596
黄鹏 <huangpeng@huangpengdeMacBook-Pro.local>
2024-01-08 15:13:33 +0800

    rm compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
-
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  //   site: "https://www.universe-hub.cn/",
-  trailingSlash: 'never',
-  integrations: [tailwind(), preact(), mdx(), compress()],
-  image: {
-    service: squooshImageService()
-  }
+	//   site: "https://www.universe-hub.cn/",
+	trailingSlash: 'never',
+	integrations: [tailwind(), preact(), mdx()],
+	image: {
+		service: squooshImageService()
+	}
 });
