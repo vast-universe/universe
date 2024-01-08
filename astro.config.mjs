c2a14abf36f63cc05ff753d5d8f06972ee706762
黄鹏 <huangpeng@huangpengdeMacBook-Pro.local>
2024-01-08 15:35:34 +0800

    compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
